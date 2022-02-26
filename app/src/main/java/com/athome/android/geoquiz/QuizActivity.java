2f8efb0b2e95edd635eae6127d864c996113fda6
seerobertcode <robert.bosoiu@yahoo.com>
2022-02-26 18:41:28 +0200

    Added Logs to onCreate, onStart, onResume, onPause, onStop and onDestroy methods

--- app/src/main/java/com/athome/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/athome/android/geoquiz/QuizActivity.java
@@ -3,6 +3,7 @@ package com.athome.android.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -27,10 +28,12 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_asia, true),
     };
     private int mCurrentIndex = 0;
+    private static final String TAG = "QuizActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         //QUESTION TEXT VIEW
@@ -72,6 +75,36 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
         updateQuestion();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     private void updateQuestion() {
