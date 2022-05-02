ede8608c01a3c953a33ecd075ce76a6759de25e1
seerobertcode <robert.bosoiu@yahoo.com>
2022-05-02 08:56:20 +0300

    Chapter 6
    -Adding activity animation code for Show Answer Button

--- app/src/main/java/com/athome/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/athome/android/geoquiz/CheatActivity.java
@@ -1,11 +1,16 @@
 package com.athome.android.geoquiz;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -43,6 +48,7 @@ public class CheatActivity extends AppCompatActivity {
 
         mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View v) {
                 if (mAnswerIsTrue) {
@@ -51,6 +57,21 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShownResult(true);
+
+                //↓↓↓ Animation for Show Answer Button ↓↓↓
+                int cx = mShowAnswerButton.getWidth() / 2;
+                int cy = mShowAnswerButton.getHeight() / 2;
+                float radius = mShowAnswerButton.getWidth();
+                Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                anim.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        mShowAnswerButton.setVisibility(View.INVISIBLE);
+                    }
+                });
+                anim.start();
+                //↑↑↑ Animation for Show Answer Button ↑↑↑
             }
         });
 
